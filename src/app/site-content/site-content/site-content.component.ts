75ca156de87ef76e3dc13fdba05ef47f09522b05
shashi97 <thetiwari.tiwari01@gmail.com>
2017-02-06 23:53:14 +0530

    Setup CKeditor config for html.

--- src/app/site-content/site-content/site-content.component.ts
+++ src/app/site-content/site-content/site-content.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { SiteContentModel } from '../shared/site-content.model';
 import { SiteContentService } from '../shared/site-content.service';
+
 @Component({
 	selector: 'site-content',
 	templateUrl: 'site-content.component.html',
@@ -11,17 +12,22 @@ export class SiteContentComponent implements OnInit, AfterViewInit {
 
 	private sitecontent: SiteContentModel = new SiteContentModel();;
 	private ContentId: number;
-	private demo: string = "<p>asdasd</p>";
+	private CkeditorConfig = {
+		allowedContent: true,
+		extraAllowedContent:'span(*)',
+		protectedSource:/<i[^>]*><\/i>/g
+	}
 	constructor(private activatedRoute: ActivatedRoute,
 		private siteContentService: SiteContentService,
 		private router: Router,
 	) {
-
+		this.sitecontent.PageContent = "<p>element</p>"
 		this.activatedRoute.params.subscribe(params => {
 			this.ContentId = +params['Id']; // (+) converts string 'id' to a number
 		});
 
 	}
+
 	ngAfterViewInit() {
 		setTimeout(() => {
 			if (this.ContentId > 0) {
@@ -37,18 +43,18 @@ export class SiteContentComponent implements OnInit, AfterViewInit {
 	}
 
 	SaveSiteContents() {
-		alert(JSON.stringify(this.sitecontent))
+		// alert(JSON.stringify(this.sitecontent))
 		this.siteContentService.saveSiteContents(this.sitecontent).then(result => {
 			this.router.navigate(['/siteContentList']);
 		});
 	}
 
 	private getSiteContentByID(): void {
-		this.siteContentService
-			.getSiteContentByID(this.ContentId)
-			.then(result => {
-				this.sitecontent = result;
-			});
-	}
+	this.siteContentService
+		.getSiteContentByID(this.ContentId)
+		.then(result => {
+			this.sitecontent = result;
+		});
+}
 
 }
